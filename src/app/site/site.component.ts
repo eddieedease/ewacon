4285edbaa9a36a0175be73dc368878b6c7a24e76
eddie <eddie@localhost.localdomain>
2017-11-25 15:35:34 +0100

    datatabletemplateinput

--- src/app/site/site.component.ts
+++ src/app/site/site.component.ts
@@ -1,6 +1,7 @@
 import {
   Component,
-  OnInit
+  OnInit,
+  ViewChild
 } from '@angular/core';
 
 import {
@@ -20,6 +21,10 @@ import {
   EwasteServiceService
 } from '../ewaste-service.service';
 
+import {
+  DatatableComponent
+} from '@swimlane/ngx-datatable';
+
 
 declare var $: any;
 
@@ -33,14 +38,49 @@ declare var $: any;
 
 export class SiteComponent implements OnInit {
 
+
+  @ViewChild(DatatableComponent) table: DatatableComponent;
+
   activeArcades;
   notActiveArcades;
+
+  // datatable vars
+  rows = [];
+  columns = [];
+  temp = [];
+  selected = [];
 
 
   // tslint:disable-next-line:max-line-length
   constructor(private serser: EwasteServiceService, private router: Router, private route: ActivatedRoute, private _sanitizer: DomSanitizer) {
     this.activeArcades = [];
     this.notActiveArcades = [];
+
+    // dummy data for tables
+    this.rows = [{
+      id: '1',
+      locatie: 'TU Einhoven',
+      aantal: '100'
+    },
+    {
+      id: '2',
+      locatie: 'Pakistan',
+      aantal: '50'
+    },
+    {
+      id: '3',
+      locatie: 'Klokhuis',
+      aantal: '70'
+    },
+  ];
+  this.columns = [
+    {
+      name: 'Locatie'
+    },
+    {
+      name: 'Aantal'
+    }
+  ];
   }
 
 
@@ -78,6 +118,117 @@ export class SiteComponent implements OnInit {
   // TODO: implement
   filterAracdes() {
 
+  }
+
+
+
+  // NOTE: Datatables functions
+  onSort(event) {
+    // event was triggered, start sort sequence
+    this.serser.debugLog('Sort Event' + event);
+
+    this.serser.debugLog(event.column.name);
+
+    const whichDir = event.newValue;
+    this.serser.debugLog('dir is' + whichDir);
+    let magicw;
+
+    switch (event.column.name) {
+      case 'Titel':
+        magicw = 'Title';
+        break;
+      case 'Bibliotheek':
+        magicw = 'Library';
+        break;
+      case 'Code':
+        magicw = 'Code';
+        break;
+      case 'Actief':
+        magicw = 'Published';
+        break;
+    }
+
+    const rows = [...this.rows];
+    // this is only for demo purposes, normally
+    // your server would return the result for
+    // you and you would just set the rows prop
+    this.serser.debugLog(this.rows);
+    // Sorting call
+    rows.sort(this.sortWhat(magicw, whichDir));
+    this.rows = rows;
+  }
+
+  /**
+   * Datables descending, ascending sorting with clicking on top column
+   */
+  sortWhat(sortwhat, _dir) {
+    if (_dir === 'desc') {
+      // Some magical sorting over here
+      let sortOrder = 1;
+      if (sortwhat[0] === '-') {
+        sortOrder = -1;
+        sortwhat = sortwhat.substr(1);
+      }
+      return function (a, b) {
+        const result = (a[sortwhat] < b[sortwhat]) ? -1 : (a[sortwhat] > b[sortwhat]) ? 1 : 0;
+        return result * sortOrder;
+      }
+    } else if (_dir === 'asc') {
+      let sortOrder2 = 1;
+      if (sortwhat[0] === '-') {
+        sortOrder2 = -1;
+        sortwhat = sortwhat.substr(1);
+      }
+      return function (a, b) {
+        const result = (b[sortwhat] < a[sortwhat]) ? -1 : (b[sortwhat] > a[sortwhat]) ? 1 : 0;
+        return result * sortOrder2;
+      }
+    }
+  }
+
+
+
+  // The updating of the libs
+   /**
+   * the Own items Searching in name, looks at the model
+   */
+  updateFilter(_value) {
+    // this.serser.debugLog("event zoek updatoz hier");
+    // this.serser.debugLog(event);
+    // this.serser.debugLog(event.target.value);
+
+    const val = _value.toLowerCase();
+
+    // filter our data
+    const temp = this.temp.filter(function (d) {
+      return d.Title.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+
+    // update the rows
+    this.rows = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.table.offset = 0;
+  }
+
+    /**
+   * the Own items Searching in library, looks at the model
+   */
+  updateFilterLib(_value) {
+    // this.serser.debugLog("event zoek updatoz hier");
+    // this.serser.debugLog(event);
+    // this.serser.debugLog(event.target.value);
+
+    const val = _value.toLowerCase();
+
+    // filter our data
+    const temp = this.temp.filter(function (d) {
+      return d.Library.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+
+    // update the rows
+    this.rows = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.table.offset = 0;
   }
 
 }
