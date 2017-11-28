e73524d9989a8d104920c5b5592659fb405c0e5f
Eddie Maas <eddie@edease.nl>
2017-11-28 19:11:07 +0100

    api works

--- src/api/index.php
+++ src/api/index.php
@@ -75,7 +75,49 @@ $app->get('/api/getall', function (Request $request, Response $response) {
 });
 
 
+$app->get('/insert/{name}', function (Request $request, Response $response) { 
 
+    
+    
+    $paramname= $request->getQueryParam('name', $default = null);
+    $paramstatus= $request->getQueryParam('status', $default = null);
+    $paramlocation= $request->getQueryParam('location', $default = null);
+    
+    $paramlonglat= $request->getQueryParam('longlat', $default = null);
+    $paramphonetot= $request->getQueryParam('phonetot', $default = null);
+    $paramphonefailed= $request->getQueryParam('phonefailed', $default = null);
+    $paramteamstot= $request->getQueryParam('teamstot', $default = null);
+
+    $paramdateplaced = $request->getQueryParam('dateplaced', $default = null);
+    $paramdateend = $request->getQueryParam('dateend', $default = null);
+    $paramdateend = $request->getQueryParam('lastused', $default = null);
+    
+    // $name = $request->getAttribute('name');
+
+    // set up the connection variables
+    include 'db.php';
+    // connect to the database
+    $dbh = new PDO("mysql:host=$hostname;dbname=$db_name", $username, $password);
+    $sql = "INSERT INTO arcades (name,status,location,longlat,phonetot,phonefailed,teamstot,dateplaced,dateend,lastused) VALUES ('$paramname','$paramstatus','$paramlocation','$paramlonglat','$paramphonetot','$paramphonefailed','$teamstot','$dateplaced','$dateend','$lastued')";
+
+     // use prepared statements, even if not strictly required is good practice
+    $stmt = $dbh->prepare($sql);
+    // execute the query
+    $dbh = null;
+    $stmt->execute();
+
+    $data = array('Jsonresponse' => 'item1', 'type' => $param1);
+    // $response = json_encode($data);
+    $response = 'Added!!!?';
+    return $response;
+    //return $newResponse;
+});
+
+
+
+
+
+// EXAMPLE
 $app->get('/api/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $data = array('Jsonresponse' => 'item1', 'type' => '40X');
@@ -83,6 +125,11 @@ $app->get('/api/{name}', function (Request $request, Response $response) {
     return $response;
     //return $newResponse;
 });
+
+
+
+
+
 
 
 $app->run();
